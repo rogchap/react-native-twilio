d5f56dbcdc6be9f9bfdde1fc11dccdaf68023a8a
Roger Chapman <rogchap@gmail.com>
2015-11-12 12:45:50 +1100

    Added twilio SDK manually as Pods where causing linker issues with a react project

---
+++ index.js
@@ -0,0 +1,5 @@
+let React = require('react-native');
+let { NativeModules } = React;
+let { Twilio } = NativeModules;
+
+module.exports = Twilio;
--- ios/RCTTwilio/RCTTwilio.m
+++ ios/RCTTwilio/RCTTwilio.m
@@ -8,6 +8,54 @@
 
 #import "RCTTwilio.h"
 
-@implementation RCTTwilio
+@implementation RCTTwilio {
+    TCDevice* _phone;
+    TCConnection* _connection;
+}
+
+RCT_EXPORT_MODULE();
+
+@synthesize bridge = _bridge;
+
+RCT_EXPORT_METHOD(initWithTokenUrl:(NSString *) tokenUrl) {
+    NSURL *url = [NSURL URLWithString: tokenUrl];
+    NSError *error = nil;
+    NSString *token = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
+    if (token == nil) {
+        NSLog(@"Error retrieving token: %@", [error localizedDescription]);
+    } else {
+        _phone = [[TCDevice alloc] initWithCapabilityToken:token delegate:self];
+    }
+}
+
+RCT_EXPORT_METHOD(connect) {
+    _connection = [_phone connect:nil delegate:nil];
+}
+
+RCT_EXPORT_METHOD(connect:(NSDictionary *) params) {
+    _connection = [_phone connect:params delegate:nil];
+}
+
+RCT_EXPORT_METHOD(disconnect) {
+    [_connection disconnect];
+}
+
+-(void)device:(TCDevice *)device didReceiveIncomingConnection:(TCConnection *)connection {
+    if (device.state == TCDeviceStateBusy) {
+        [connection reject];
+    } else {
+        [connection accept];
+        _connection = connection;
+    }
+}
+
+-(void)deviceDidStartListeningForIncomingConnections:(TCDevice*)device {
+    NSLog(@"Device: %@ deviceDidStartListeningForIncomingConnections", device);
+}
+
+-(void)device:(TCDevice *)device didStopListeningForIncomingConnections:(NSError *)error {
+    NSLog(@"Device: %@ didStopListeningForIncomingConnections: %@", device, error);
+}
+
 
 @end
